2f30ef896aeea8225c66e6371ec972da81b79572
sontrandtu <67112723+sontrandtu@users.noreply.github.com>
2021-09-21 11:25:48 +0700

    Create theme.dart

---
+++ lib/constants/theme.dart
@@ -0,0 +1,11 @@
+import 'package:cdio4/constants/config.dart';
+import 'package:flutter/material.dart';
+
+class CustomTheme {
+  static ThemeData get lightTheme {
+    return ThemeData(
+      primaryColor: kPrimaryColor,
+      fontFamily: 'Muli',
+    );
+  }
+}
